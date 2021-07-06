f1df8c7992d58b07f2b7286f3cb9402693a31fc0
NguyenVanPhat060800 <86821260+NguyenVanPhat060800@users.noreply.github.com>
2021-07-06 16:30:54 +0700

    Initial commit

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,52 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public Course ObjCourse { get; private set; }
+
+        // GET: Courses
+        public ActionResult Create()
+        {
+            BigSchoolConText contex = new BigSchoolConText();
+            Course ObjCourse = new Course();
+            ObjCourse.ListCategory = contex.Categories.ToList();
+
+
+            return View(ObjCourse);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Course objCourse)
+        {
+            BigSchoolConText contex = new BigSchoolConText();
+            ModelState.Remove("LecturerId");
+            if (!ModelState.IsValid)
+            {
+                objCourse.ListCategory = contex.Categories.ToList();
+                return View("Create", objCourse);
+            }
+
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            objCourse.LecturerId = user.Id;
+
+            contex.Courses.Add(ObjCourse);
+            contex.SaveChanges();
+
+
+
+
+
+            return RedirectToAction("index", "Home");
+        }
+    }
+}
---
+++ Models/BigSchoolConText.cs
@@ -0,0 +1,53 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace BigSchool.Models
+{
+    public partial class BigSchoolConText : DbContext
+    {
+        public BigSchoolConText()
+            : base("name=BigSchoolConText")
+        {
+        }
+
+        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
+        public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
+        public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
+        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
+        public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AspNetRole>()
+                .HasMany(e => e.AspNetUsers)
+                .WithMany(e => e.AspNetRoles)
+                .Map(m => m.ToTable("AspNetUserRoles").MapLeftKey("RoleId").MapRightKey("UserId"));
+
+            modelBuilder.Entity<AspNetUser>()
+                .HasMany(e => e.AspNetUserClaims)
+                .WithRequired(e => e.AspNetUser)
+                .HasForeignKey(e => e.UserId);
+
+            modelBuilder.Entity<AspNetUser>()
+                .HasMany(e => e.AspNetUserLogins)
+                .WithRequired(e => e.AspNetUser)
+                .HasForeignKey(e => e.UserId);
+
+            modelBuilder.Entity<AspNetUser>()
+                .HasMany(e => e.Courses)
+                .WithRequired(e => e.AspNetUser)
+                .HasForeignKey(e => e.LecturerId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Category>()
+                .HasMany(e => e.Courses)
+                .WithRequired(e => e.Category)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
